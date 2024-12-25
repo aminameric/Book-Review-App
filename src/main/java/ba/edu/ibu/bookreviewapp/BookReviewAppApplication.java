707fa681ae19397aa9b0d604b347304c739d93f3
Amina Merić <116023819+aminameric@users.noreply.github.com>
2024-12-25 23:23:59 +0100

    fixing error

--- src/main/java/ba/edu/ibu/bookreviewapp/BookReviewAppApplication.java
+++ src/main/java/ba/edu/ibu/bookreviewapp/BookReviewAppApplication.java
@@ -1,5 +1,6 @@
 package ba.edu.ibu.bookreviewapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookReviewAppApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("GOOGLE_BOOKS_API_KEY", dotenv.get("GOOGLE_BOOKS_API_KEY"));
         SpringApplication.run(BookReviewAppApplication.class, args);
     }
 
