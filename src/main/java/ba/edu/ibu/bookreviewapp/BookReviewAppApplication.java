31dc1221fa1dfe6ec85468495745ee3b118e1de2
Amina Merić <116023819+aminameric@users.noreply.github.com>
2025-01-09 09:59:35 +0100

    deployment done

--- src/main/java/ba/edu/ibu/bookreviewapp/BookReviewAppApplication.java
+++ src/main/java/ba/edu/ibu/bookreviewapp/BookReviewAppApplication.java
@@ -1,6 +1,5 @@
 package ba.edu.ibu.bookreviewapp;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,23 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookReviewAppApplication {
 
     public static void main(String[] args) {
-        // Load the API key from environment variables if present (Render setup)
-        String googleBooksApiKey = System.getenv("GOOGLE_BOOKS_API_KEY");
-
-        // If not present, try loading from the .env file (for local development)
-        if (googleBooksApiKey == null || googleBooksApiKey.isEmpty()) {
-            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-            googleBooksApiKey = dotenv.get("GOOGLE_BOOKS_API_KEY");
-        }
-
-        // Set the property if the key was found
-        if (googleBooksApiKey != null && !googleBooksApiKey.isEmpty()) {
-            System.setProperty("GOOGLE_BOOKS_API_KEY", googleBooksApiKey);
-        } else {
-            throw new RuntimeException("Google Books API Key is missing!");
-        }
-
         SpringApplication.run(BookReviewAppApplication.class, args);
     }
-
 }
